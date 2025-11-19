d66bb6c7ad2280a72580e467aa6d17a3a403ab43
douglas <douglas.costa@jogoglobal.com>
2025-11-19 19:18:13 -0300

    Robotic Improvement

--- src/controllers/package_controller.py
+++ src/controllers/package_controller.py
@@ -2,12 +2,37 @@ from fastapi import Depends, APIRouter
 from src.services.package_sorter import PackageSorterService
 from src.domain.package_request import PackageRequest
 
-
 router = APIRouter(prefix="/package", tags=["Package"])
 
+csv_file_name = "packages.csv"
 
 def get_package_sorter_service():
     return PackageSorterService()
+
+def get_statistics(df, name=None, number_of_artifacts=None):
+    print(f"Number of {name} packages: {len(df)}")
+    print(f"Percentage {name} of total: {len(df) / number_of_artifacts * 100:.2f}%")
+    print(f"{name} Volume Mean:{df['Volume'].mean()}")
+    print(f"{name} Volume Min: {df['Volume'].min()}")
+    print(f"{name} Volume Max: {df['Volume'].max()}")
+    print(f"{name} Mass Mean: {df['Mass'].mean()}")
+    print(f"{name} Mass Min: {df['Mass'].min()}")
+    print(f"{name} Mass Max: {df['Mass'].max()}")
+
+@router.post("/process/file")
+def process_file(service: PackageSorterService = Depends(get_package_sorter_service)):
+    # Apply the read logic and proccessment
+    df = service.get_df(csv_file_name)
+    df["Classification"] = df.apply(service.sort_by_row, axis=1)
+    df["Volume"] = df.apply(service.get_volume, axis=1)
+    total_artifact = len(df)
+    standard_df = df[df["Classification"] == "STANDARD"]
+    special_df = df[df["Classification"] == "SPECIAL"]
+    rejected_df = df[df["Classification"] == "REJECTED"]
+
+    get_statistics(standard_df, "STANDARD", total_artifact)
+    get_statistics(special_df, "SPECIAL", total_artifact)
+    get_statistics(rejected_df, "REJECTED", total_artifact)
 
 
 @router.post("/sort")
--- src/services/package_sorter.py
+++ src/services/package_sorter.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class PackageSorterService:
     def __init__(self, max_volume=1_000_000, max_dimension=150, max_mass=20):
         self.MAX_VOLUME = max_volume
@@ -23,6 +25,32 @@ class PackageSorterService:
             return True
 
         return False
+
+    def get_df(self, filename: str):
+        df = pd.read_csv(filename, on_bad_lines='skip')
+        # Convert everything to numeric (integers), coercing errors to NaN
+        df = df.apply(pd.to_numeric, errors='coerce')
+
+        # Drop rows or columns with NaN
+        df = df.dropna()  # drops rows with any NaN
+        df = df.astype(int)  # finally convert to int
+        df = df[(df > 0).all(axis=1)]
+        return df
+
+
+    def get_volume(self, row):
+        width = row["Width"]
+        height = row["Height"]
+        length = row["Length"]
+        return self._calculate_volume(width, height, length)
+
+    def sort_by_row(self, row):
+        width = row["Width"]
+        height = row["Height"]
+        length = row["Length"]
+        mass = row["Mass"]
+
+        return self.sort(int(width), int(height), int(length), int(mass))
 
     def sort(self, width, height, length, mass):
         bulky = self._package_is_bulky(width, height, length)
